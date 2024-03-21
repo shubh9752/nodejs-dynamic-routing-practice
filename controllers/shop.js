55d2843e77b2b9d287cb3c8a7f2f677c3b6d5ffd
shubh9752 <shubhamkumar3.sk@gmail.com>
2024-03-22 04:46:58 +0530

    quary routs

--- controllers/shop.js
+++ controllers/shop.js
@@ -11,8 +11,8 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-exports.getProduct=(req,res,next)=>{
-  const prodId=(req.params.productId);
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId, product => {
     res.render('shop/product-detail', {
       product: product,
@@ -20,7 +20,7 @@ exports.getProduct=(req,res,next)=>{
       path: '/products'
     });
   });
-}
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -33,9 +33,23 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts
+      });
+    });
   });
 };
 
@@ -45,6 +59,14 @@ exports.postCart = (req, res, next) => {
     Cart.addProduct(prodId, product.price);
   });
   res.redirect('/cart');
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  });
 };
 
 exports.getOrders = (req, res, next) => {
